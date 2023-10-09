1c29398302788127d386e3b24948a1ea736a437b
DouglasLindahl <douglas.lindahl@gmail.com>
2023-10-09 11:15:10 +0200

    removed weekly view button

--- src/components/navbar/page.js
+++ src/components/navbar/page.js
@@ -15,16 +15,13 @@ export default function Navbar(props) {
     };
     return (
       <div className="fixed bottom-0 left-0 w-full text-white py-4">
-        <div className="container mx-auto flex justify-between items-center">
+        <div className="container mx-auto flex justify-center items-center">
           <button onClick={updateView} className="bg-purple-500 hover:bg-purple-600 text-white font-semibold px-6 py-3 rounded-full mx-2">
             Change View
           </button>
           <Link href="addEvent" className="bg-green-500 hover:bg-green-600 text-white font-semibold w-14 h-14 flex items-center justify-center rounded-full">
             +
           </Link>
-          <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-6 py-3 rounded-full mx-2">
-            Weekly View
-          </button>
         </div>
       </div>
     );
