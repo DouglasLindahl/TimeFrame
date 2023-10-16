e9aca4a8f286394a42fdd339b36a65ecaae726f9
DouglasLindahl <douglas.lindahl@gmail.com>
2023-10-16 06:44:01 +0200

    color update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,23 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'TimeFrame',
-  description: '',
+  title: "TimeFrame",
+  description: "",
   manifest: "/manifest.json",
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-            <head>
+      <head>
+        <meta name="theme-color" content="#6C63FF" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" type="image/x-icon" href="/logo/192x192.png" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
