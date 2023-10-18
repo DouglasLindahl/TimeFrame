b07b4159992039580120eb2b0b199bb164c9706b
DouglasLindahl <douglas.lindahl@gmail.com>
2023-10-18 14:37:37 +0200

    changed addEvent page styling

--- src/app/authenticated/addEvent/page.js
+++ src/app/authenticated/addEvent/page.js
@@ -4,11 +4,54 @@ import HomeHeader from "@/components/header/page";
 import Navbar from "@/components/navbar/page";
 import { supabase } from "../../../../supabase";
 import styled from "styled-components";
+import { useQuery } from "graphql-hooks";
+
+const COLOR_QUERY = `
+query{
+  main {
+    logo{
+      url
+    }
+    shadowColor{
+      hex
+    }
+    primaryColor{
+      hex
+    }
+    textColor{
+      hex
+    }
+    secondaryColor{
+      hex
+    }
+    backgroundPrimary{
+      hex
+    }
+    backgroundSecondary{
+      hex
+    }
+    currentDay{
+      hex
+    }
+    currentDayText{
+      hex
+    }
+    offDay{
+      hex
+    }
+    offDayText{
+      hex
+    }
+  }
+}
+`;
 
 const Container = styled.section`
   height: 100vh;
   display: flex;
   flex-direction: column;
+  background-color: ${(props) => props.backgroundcolor};
+  color: ${(props) => props.textcolor};
 `;
 const FormContainer = styled.div`
   width: 100%;
@@ -28,7 +71,6 @@ const Form = styled.form`
     display: block;
     font-size: 0.875rem;
     font-weight: 600;
-    color: #606060;
   }
 
   input,
@@ -41,6 +83,7 @@ const Form = styled.form`
     border-radius: 0.25rem;
     outline: none;
     transition: border 0.2s;
+    color: ${(props) => props.textcolor};
 
     &:focus {
       border: 1px solid #63b3ed;
@@ -50,28 +93,26 @@ const Form = styled.form`
   textarea {
     resize: vertical;
   }
+`;
 
-  button {
+const Button = styled.button`
     width: 100%;
-    background-color: #3182ce;
+    background-color: ${(props) => props.primary};
     color: white;
     padding: 0.5rem 0;
     border-radius: 0.25rem;
     margin-top: 1rem;
     cursor: pointer;
-
-    &:hover {
-      background-color: #4299e1;
-    }
-  }
 `;
 
 const StyledInput = styled.input`
   height: 2.5rem;
+  color: ${(props) => props.textcolor};
 `;
 
 const StyledTextArea = styled.textarea`
   min-height: 3rem;
+  color: ${(props) => props.textcolor};
 `;
 
 const StyledSelect = styled.select`
@@ -84,6 +125,13 @@ export default function AddEvent() {
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
   const [color, setColor] = useState("");
+
+  const { data, loading, error } = useQuery(COLOR_QUERY);
+
+  const backgroundPrimary = data?.main?.backgroundPrimary?.hex || "303030";
+  const backgroundSecondary = data?.main?.backgroundSecondary?.hex || "303030";
+  const primaryColor= data?.main?.primaryColor?.hex || "303030";
+  const textColor = data?.main?.textColor?.hex || "303030";
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -104,11 +152,11 @@ export default function AddEvent() {
   };
 
   return (
-    <Container>
+    <Container textcolor={textColor} backgroundcolor={backgroundPrimary}>
       <HomeHeader header={"home"}></HomeHeader>
       <FormContainer>
-        <Title>Add Event</Title>
-        <Form onSubmit={handleSubmit}>
+        <Title>Create Event</Title>
+        <Form backgroundcolor={backgroundPrimary} onSubmit={handleSubmit}>
           <div className="mb-4">
             <label htmlFor="title">Title</label>
             <StyledInput
@@ -117,6 +165,7 @@ export default function AddEvent() {
               name="title"
               value={title}
               onChange={(e) => setTitle(e.target.value)}
+              textcolor={backgroundPrimary}
               required
             />
           </div>
@@ -127,6 +176,7 @@ export default function AddEvent() {
               name="description"
               value={description}
               onChange={(e) => setDescription(e.target.value)}
+              textcolor={backgroundPrimary}
               rows="4"
             ></StyledTextArea>
           </div>
@@ -138,6 +188,7 @@ export default function AddEvent() {
               name="date"
               value={date}
               onChange={(e) => setDate(e.target.value)}
+              textcolor={backgroundPrimary}
               required
             />
           </div>
@@ -149,6 +200,7 @@ export default function AddEvent() {
               name="time"
               value={time}
               onChange={(e) => setTime(e.target.value)}
+              textcolor={backgroundPrimary}
               required
             />
           </div>
@@ -160,10 +212,11 @@ export default function AddEvent() {
               name="color"
               value={color}
               onChange={(e) => setColor(e.target.value)}
+              textcolor={backgroundPrimary}
               required
             />
           </div>
-          <button type="submit">Submit</button>
+          <Button backgroundcolor={backgroundSecondary} primary={primaryColor} type="submit">Submit</Button>
         </Form>
       </FormContainer>
       <Navbar navbar="home"></Navbar>
