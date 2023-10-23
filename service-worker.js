9c5cc08862c54e10e10bf9a8797faecbd6bb167f
DouglasLindahl <douglas.lindahl@gmail.com>
2023-10-23 11:49:31 +0200

    test

--- service-worker.js
+++ service-worker.js
@@ -15,19 +15,32 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        // Serve cached assets if available, or fetch from network
-        return (
-          response ||
-          fetch(event.request).then((fetchResponse) => {
-            // Cache the fetched response for future use
-            return caches.open(cacheVersion).then((cache) => {
-              cache.put(event.request, fetchResponse.clone());
-              return fetchResponse;
-            });
-          })
-        );
-      })
-    );
-  });
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Serve cached assets if available, or fetch from network
+      return (
+        response ||
+        fetch(event.request).then((fetchResponse) => {
+          // Cache the fetched response for future use
+          return caches.open(cacheVersion).then((cache) => {
+            cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+          });
+        })
+      );
+    })
+  );
+});
+
+// Push Notification Handler
+self.addEventListener("push", (event) => {
+  const options = {
+    body: event.data.text(),
+    icon: "/logo/Logomark_TimeFrame_Vit_T.png",
+    badge: "/logo/Logomark_TimeFrame_Vit_T.png",
+  };
+
+  event.waitUntil(
+    self.registration.showNotification("Push Notification Title", options)
+  );
+});
--- src/components/navbar/page.js
+++ src/components/navbar/page.js
@@ -141,15 +141,35 @@ export default function Navbar(props) {
     checkProfile();
   }, []);
 
-  function sendNotification(){
-    Notification.requestPermission().then(perm => {
-      if(perm === "granted"){
-        const notification = new Notification("Example notification", {
-          body: "test"
-        })
-      }
-    })
+  function sendNotification() {
+    if ("Notification" in window) {
+      // Check if the Notification API is available in the browser
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          // Permission granted, you can send a push notification
+          const options = {
+            body: "This is your push notification message.",
+            icon: "/logo/Logomark_TimeFrame_Vit_T.png",
+            badge: "/logo/Logomark_TimeFrame_Vit_T.png",
+          };
+  
+          // Use the Service Worker to show the notification
+          navigator.serviceWorker.ready.then((registration) => {
+            registration.showNotification("Push Notification Title", options);
+          });
+        } else if (permission === "denied") {
+          // Permission denied by the user
+          console.log("Notification permission denied.");
+        } else if (permission === "default") {
+          // The user closed the permission dialog without granting or denying
+          console.log("Notification permission dialog closed without a decision.");
+        }
+      });
+    } else {
+      console.log("Notification API is not available in this browser.");
+    }
   }
+  
 
   const removeEvent = async () => {
     const { error } = await supabase.from("Events").delete().eq("id", props.id);
